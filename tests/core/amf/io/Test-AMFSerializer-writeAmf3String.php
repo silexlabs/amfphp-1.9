643176c132f52ebb98f9f393e7923f2e6a163aeb
ci-dev <ci-dev@d1a04505-e93a-0410-bb8f-ef7208cba7c8>
2010-10-14 11:15:20 +0000

    Added test for serialisation of long strings (>64 byte).
    git-svn-id: https://amfphp.svn.sourceforge.net/svnroot/amfphp/trunk@29 d1a04505-e93a-0410-bb8f-ef7208cba7c8

--- tests/core/amf/io/Test-AMFSerializer-writeAmf3String.php
+++ tests/core/amf/io/Test-AMFSerializer-writeAmf3String.php
@@ -47,4 +47,27 @@ $oAMFSerializer->writeAmf3String('This is a test!', FALSE);
 if ($oAMFSerializer->outBuffer !== pack('C', 31) . 'This is a test!' . pack('C', 0)) {
     echo 'Failed at line ' . __LINE__ . '!' . "\n";
 }
+
+$oAMFSerializer = new Wrapper_AMFSerializer();
+
+// A long string is used. If the string is longer than 127 byte, the length
+// needs to be encoded in another way, as U29-1 is too short.
+// This method is not supposed to output the value type.
+
+// "U29S-value" in "U29-1" (128 << 1 | 1 = 0x101 => 0x02 0x01 => 0x82 0x01)
+// <string>
+
+$sString = str_repeat('*', 128);
+$oAMFSerializer->writeAmf3String($sString, FALSE);
+$sBuffer = $oAMFSerializer->outBuffer;
+
+if ($sBuffer !== pack('CC', 130, 1) . $sString) {
+    echo 'Failed at line ' . __LINE__ . '!' . "\n";
+
+    for ($i = 0, $j = strlen($sBuffer); $i < $j; ++$i) {
+        echo str_pad(dechex(ord($sBuffer{$i})), 2, '0', STR_PAD_LEFT) . (ctype_print($sBuffer{$i}) ? '(' . $sBuffer{$i} . ')' : '') . ' ';
+    }
+
+    echo "\n";
+}
 ?>
